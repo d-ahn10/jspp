0c502d5123d7a777b24cd64e427aa5c2ac16d46d
David Ahn <dja1990@gmail.com>
2018-12-13 18:58:24 -0800

    pseudocode done for pp08

---
+++ problem08.js
@@ -0,0 +1,18 @@
+// Given the following function call:
+
+// character_convert('the cat in the hat');
+// And the resulting output:
+
+// "11610410132999711632105110321161041013210497116";
+// Create a function definition that achieves the output with the given input.
+
+// Optimization: How would you speed up the processing if you already encountered a value?
+
+/* End of Instructions */
+
+/* pseudocode */
+// go through the words in the sentence letter by letter
+// at each letter grab and store the ASCII value
+// add that ASCII value into the output/result sentence
+// print out the final output/result
+
